81ee64837bd8eb08c219759c5b5449567bfe557f
mrsurprise123 <44513517+mrsurprise123@users.noreply.github.com>
2020-05-02 20:00:04 -0400

    Update Project4_HMM_Dice.py

--- Project4/Project4_HMM_Dice.py
+++ Project4/Project4_HMM_Dice.py
@@ -6,6 +6,13 @@ class Data:
         self.emissionProbs = emissionProbs
         self.emissions = emissions
         self.notkeepDice = (1 - self.keepDice) / 2
+        self.changeDice = {}
+        for i in range(0,3):
+            for j in range(0,3):
+                if i == j:
+                    self.changeDice[(i,j)] = self.keepDice
+                else:
+                    self.changeDice[(i,j)] = self.notkeepDice
     def DataPrint(self):
         print(self.keepDice)
         print(self.emissionProbs)
@@ -34,48 +41,37 @@ def main():
     print("Proejct4_YihanChen")
     # filename = input("Please input the file name: ")
     data = readData("./01.txt")
-    # data.DataPrint()
+    data.DataPrint()
     dices = [0, 1, 2]
-    start_p = [1.0 / 3, 1.0 / 3, 1.0 / 3]
-    trans_p = {}
-    emis_p = {}
-    for i in range(0,3):
-        for j in range(0,3):
-            if i == j:
-                trans_p[(i,j)] = data.keepDice
-            else:
-                trans_p[(i,j)] = data.notkeepDice
-            emis_p[(i,j)] = data.emissionProbs[(i,j)]
-    maxProb = {}
+    start_prob = [1.0 / 3, 1.0 / 3, 1.0 / 3]
+    Probs = {}
     path = [[],[],[]]
+    prob = -1.0
+    dice = 0
+    sequence = []
     for dice in dices:
-        maxProb[(dice,0)] = start_p[dice] * emis_p[(dice,data.emissions[0]-1)]
+        Probs[(dice,0)] = start_prob[dice] * data.emissionProbs[(dice,data.emissions[0]-1)]
         path[dice].append(dice)
-    
     for j in range(1,len(data.emissions)):
         temp = [[],[],[]]
         for i in dices:
-            prob = - 1.0
+            tempprob = - 1.0
             for x in dices:
-                newProbs = maxProb[(x, j -1)] * trans_p[(x, i)] * emis_p[(i,data.emissions[j]-1)]
-                if newProbs > prob:
-                    prob = newProbs
-                    curDice = x
-                    maxProb[(i,j)] = prob
-                    temp[i] = path[curDice][0:j]
+                newProbs = Probs[(x, j -1)] * data.changeDice[(x, i)] * data.emissionProbs[(i,data.emissions[j]-1)]
+                if newProbs > tempprob:
+                    tempprob = newProbs
+                    Probs[(i,j)] = tempprob
+                    temp[i] = path[x][0:j]
                     temp[i].append(i)
         path = temp
-    prob = -1.0
-    dice = 0
     for i in dices:
-        if maxProb[(i,len(data.emissions) - 1)] > prob:
-            prob = maxProb[(i,len(data.emissions) - 1)]
+        if Probs[(i,len(data.emissions) - 1)] > prob:
+            prob = Probs[(i,len(data.emissions) - 1)]
             dice = i
-    result = []
     for i in path[dice]:
-        result.append(i+1)
+        sequence.append(i+1)
     print("Most possible dices sequence:")
-    print(result)
+    print(sequence)
     print("Probability of path:")
     print(prob)
 if __name__ == '__main__':
